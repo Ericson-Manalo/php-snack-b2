ee168dca3d6d96062d21c1fc6fa8d508d2e908a9
Ericson-Manalo <ericmana98@gmail.com>
2023-07-03 13:15:48 +0200

    fixed the code

--- index.php
+++ index.php
@@ -2,26 +2,34 @@
 - name sia più lungo di 3 caratteri,
 - mail contenga un punto e una chiocciola e
 - age sia un numero.
-
 Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato"-->
 
 <form action="" method="GET">
     <input type="text" name="name" id="name">
     <input type="email" name="mail" id="mail">
     <input type="text" name="age" id="age">
+    <input type="submit" value="check">
 </form>
 
-<?php 
-
-if (count($_GET['name']) >= 3){
-    echo 'Name must be at least 4 characters';
-}else{
-    if(in_array($_GET['mail'], '@') && is_numeric($_GET['age'])){
-        echo 'Accesso riuscito';
-    }else{
-        echo 'Accesso negato';
-    }
-}
+<?php
+        $accessDenied = 'Access denied';
+        $accessGranted = 'Access granted';
 
 
-?>
+        if ( empty($_GET['name'])
+        || empty($_GET['mail'])
+        || empty($_GET['age'])){
+            echo 'Please type a name, an email and an age';
+        } else {
+
+            if (!strlen($_GET['name']) > 3){
+                echo $accessDenied;
+            } elseif (! (str_contains($_GET['mail'], '@') &&  str_contains($_GET['mail'], '.')) ){
+                echo $accessDenied;
+            } elseif (!is_numeric($_GET['age'])){
+                echo $accessDenied;
+            } else {
+                echo $accessGranted;
+            }
+        }
+    ?>
